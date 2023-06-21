60a4217e163af1677d03b150fd69bbba563f6ccd
소울치킨 <soulfever01@naver.com>
2023-06-21 13:16:09 +0900

    Feat: 회원가입, 로그인 페이지 구현 (#4)
    * Feat: LoginPage 구현
    
    - 로그인 성공시 Main 페이지로
    - 실패시 alert
    
    * Feat: Signup 구현
    
    * Feat: 로그인 여부 확인 API 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import styles from './App.module.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         {/* 다른 경로에 대한 라우트 설정 */}
         <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} /> 
+        <Route path="/signup" element={<SignupPage />} /> 
         <Route path="/*" element={<NotFoundPage />} />
       </Routes>
   </Router>
