8bb6b2faf757cd2b473a795dceb80fbd86f89932
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-06-21 12:40:46 +0900

    Feat: 게시글 삭제 api 구현

--- routes/post.js
+++ routes/post.js
@@ -28,4 +28,39 @@ router.post('/', (req, res) => {
         });
 });
 
+// 게시글 삭제
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    // 세션에서 유저 정보 확인
+    const { userId } = req.session;
+    if (!userId) {
+        res.status(401).json({ error: 'User not authenticated' });
+        return;
+    }
+
+    const post = await Post.findByPk(id);
+    if (!post) {
+    res.status(404).json({ error: 'Post not found' });
+    return;
+    }
+
+    // 게시글 작성자와 세션의 유저가 일치하는지 확인
+    if (post.userId !== userId) {
+        res.status(403).json({ error: 'User not authorized' });
+        return;
+    }
+
+    // 진짜로 DB에서 게시글 삭제
+    post.destroy()
+        .then(() => {
+        res.status(204).json({ message: 'Post deleted successfully' });
+        })
+        .catch((error) => {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+        });
+});
+
+
 module.exports = router;
