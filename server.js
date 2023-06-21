5d4bc7389cafc511e4206e756090745466389a65
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-06-21 11:28:57 +0900

    Feat: 기본페이지, notfound페이지 라우팅

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.get('/', (req, res) => {
     req.sendFile(path.join(__dirname, '/build/index.html'));
 })
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import styles from './App.module.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={styles.container}>
+    <Router>
+      <Routes>
+        {/* 다른 경로에 대한 라우트 설정 */}
+        <Route path="/" element={<MainPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+  </Router>
+  </div>
   );
 }
 
